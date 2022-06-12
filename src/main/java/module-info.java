16d3becc1004d7b403eb97ddb9bee5f6ea50e915
phatnm-student-hcmus <19126008@student.hcmus.edu.vn>
2022-06-12 19:45:33 +0700

    init Maven javaFX enote server project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module pck.enoteserver {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens pck.enote_server to javafx.fxml;
+    exports pck.enote_server;
+}
---
+++ src/main/java/pck/enote_server/Main.java
@@ -0,0 +1,7 @@
+package pck.enote_server;
+
+public class Main {
+    public static void main(String[] args) {
+        Server.main(args);
+    }
+}
